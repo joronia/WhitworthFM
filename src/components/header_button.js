3c93aa32298b524f7073d1e42d5ec52214c3c920
Derik Oliver <derik.oliver@gmail.com>
2017-03-26 13:23:24 -0700

    Added skeleton components

---
+++ src/components/header_button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HeaderButton extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  remder() {
+    return (
+      // TODO: ADD JSX CODE
+    )
+  }
+}
