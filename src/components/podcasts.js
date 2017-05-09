1432465f1377ee0d5114262e8c7aece46b9cf965
Jose A Oronia <jaoronia@gmail.com>
2017-05-08 19:58:47 -0700

    Added link to SoundCloud inside podcasts

--- src/components/podcasts.js
+++ src/components/podcasts.js
@@ -12,7 +12,10 @@ class Podcasts extends Component {
     return(
 	  <div>	    
         <div className="podcasts">
-          
+          <div className="podcastsDesc">
+          <h3> Listen to our podcasts and follow us on SoundCloud </h3>
+            <h2 id="toPodcast"> <a href="https://soundcloud.com/whitworth-fm" target="_blank"> Take me to WhitworthFM SoundCloud </a></h2>
+          </div>
         </div>
 	  </div>
     );
