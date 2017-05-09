d0b4a7d8b59bd39e5e548fc717c4900c09efcc2c
Jose A Oronia <jaoronia@gmail.com>
2017-05-09 16:25:43 -0700

    Fixed facebook div

--- src/components/on_the_air.js
+++ src/components/on_the_air.js
@@ -10,21 +10,20 @@ class OnTheAir extends Component {
 
   render() {
     return(
-      <div className="on-the-air">
+    <div className="on-the-air">
         <div className="player">
-		<div className="radio-feed">
-		<iframe src="//p3.radiocdn.com/files/html/d6a571353c3e62a4d6eb98fc67d06746a84ae945.html" width="415px" height="135px" frameborder="0" scrolling="no" id="wavestreamingPlayer" frameBorder="0"></iframe>
-		</div>
-		<div className="facebookFeed">
-		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWhitworthfm&tabs=timeline&width=415&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="415px" height="90%"></iframe>
-	</div>
-	
-		</div> 
+		      <div className="radio-feed">
+		        <iframe src="//p3.radiocdn.com/files/html/d6a571353c3e62a4d6eb98fc67d06746a84ae945.html" width="415px" height="135px" frameborder="0" scrolling="no" id="wavestreamingPlayer" frameBorder="0"></iframe>
+		      </div>
+		      <div className="facebookFeed">
+		        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWhitworthfm&tabs=timeline&width=415&height=550&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="415px" height="90%"></iframe>
+	        </div>
+        </div>
         <div className="schedule"> 
-		  <h1> Schedule </h1>
+		      <h1> Schedule </h1>
           <Schedule />
         </div>
-      </div>
+    </div>
     );
   }
 }
