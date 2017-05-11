c951af9607ffa92f88824e06925d3d597b7b6be1
Riley Sexton <Riles175@gmail.com>
2017-05-11 16:01:56 -0700

    Repaired things, broke things, then fixed them again

--- src/components/on_the_air.js
+++ src/components/on_the_air.js
@@ -11,17 +11,17 @@ class OnTheAir extends Component {
   render() {
     return(
     <div className="on-the-air">
-        <div className="player">
-		      <div className="radio-feed">
-		        <iframe src="//p3.radiocdn.com/files/html/d6a571353c3e62a4d6eb98fc67d06746a84ae945.html" width="415px" height="100%" frameborder="0" scrolling="no" id="wavestreamingPlayer" frameBorder="0"></iframe>
-		      </div>
-		      <div className="facebookFeed">
-		        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWhitworthfm&tabs=timeline&width=415&height=550&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="415px" height="100%"></iframe>
-	        </div>
-        </div>
-        <div className="schedule"> 
-          <Schedule />
-        </div>
+      <div className="player">
+		    <div className="radio-feed">
+		      <iframe src="//p3.radiocdn.com/files/html/d6a571353c3e62a4d6eb98fc67d06746a84ae945.html" width="415px" height="100%" frameborder="0" scrolling="no" id="wavestreamingPlayer" frameBorder="0"></iframe>
+		    </div>
+		    <div className="facebookFeed">
+		      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWhitworthfm&tabs=timeline&width=415&height=550&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="415px" height="100%"></iframe>
+	      </div>
+      </div>
+      <div className="schedule"> 
+        <Schedule />
+      </div>
     </div>
     );
   }
